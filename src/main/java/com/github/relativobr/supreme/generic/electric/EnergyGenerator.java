0da8fdae88f17e177479f6371362a3ff6ba9c309
iLuckyx <asmith0690@hotmail.com>
2026-03-21 03:01:02 -0400

    Fixing energy

--- src/main/java/com/github/relativobr/supreme/generic/electric/EnergyGenerator.java
+++ src/main/java/com/github/relativobr/supreme/generic/electric/EnergyGenerator.java
@@ -1,23 +1,28 @@
 package com.github.relativobr.supreme.generic.electric;
 
 import com.github.relativobr.supreme.Supreme;
+import com.github.relativobr.supreme.util.UtilEnergy;
 import io.github.thebusybiscuit.slimefun4.api.items.ItemGroup;
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
 import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
 import io.github.thebusybiscuit.slimefun4.implementation.Slimefun;
-import io.github.thebusybiscuit.slimefun4.implementation.items.electric.AbstractEnergyProvider;
+import io.github.thebusybiscuit.slimefun4.core.attributes.EnergyNetProvider;
+import io.github.thebusybiscuit.slimefun4.libraries.dough.items.CustomItemStack;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.logging.Level;
 import javax.annotation.Nonnull;
-
 import me.mrCookieSlime.CSCoreLibPlugin.Configuration.Config;
 import me.mrCookieSlime.Slimefun.api.BlockStorage;
+import me.mrCookieSlime.Slimefun.api.inventory.BlockMenu;
+import me.mrCookieSlime.Slimefun.api.inventory.BlockMenuPreset;
+import me.mrCookieSlime.Slimefun.api.inventory.DirtyChestMenu;
+import net.guizhanss.guizhanlib.slimefun.machines.MenuBlock;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 
-public final class EnergyGenerator extends AbstractEnergyProvider {
+public final class EnergyGenerator extends MenuBlock implements EnergyNetProvider {
 
   private static final String GENERATION_KEY = "supreme-generator-output";
   private static final String GENERATION_DELAY_KEY = "supreme-generator-delay";
@@ -52,19 +57,21 @@ public final class EnergyGenerator extends AbstractEnergyProvider {
     return this;
   }
 
-  @Nonnull
   @Override
-  public ItemStack getProgressBar() {
-    return new ItemStack(Material.OBSERVER);
+  protected void setup(BlockMenuPreset blockMenuPreset) {
+    blockMenuPreset.drawBackground(
+        new int[]{
+            0, 1, 2, 3, 4, 5, 6, 7, 8,
+            9, 10, 11, 12, 13, 14, 15, 16, 17,
+            18, 19, 20, 21, 22, 23, 24, 25, 26
+        }
+    );
   }
 
   @Override
-  public int getEnergyProduction() {
-    return this.energy;
+  protected int[] getInputSlots(DirtyChestMenu dirtyChestMenu, ItemStack itemStack) {
+    return new int[0];
   }
-
-  @Override
-  protected void registerDefaultFuelTypes() {}
 
   @Override
   public int getGeneratedOutput(Location l, Config data) {
@@ -85,6 +92,7 @@ public final class EnergyGenerator extends AbstractEnergyProvider {
       generatorData.setValue(GENERATION_DELAY_KEY, String.valueOf(currentDelay));
     }
 
+    updateMenu(l, generatorData, generate);
     logDebugState(l, generatorData, generate, currentDelay);
 
     return generate;
@@ -103,6 +111,40 @@ public final class EnergyGenerator extends AbstractEnergyProvider {
   @Override
   public int[] getOutputSlots() {
     return new int[0];
+  }
+
+  private void updateMenu(Location location, Config data, int generated) {
+    BlockMenu menu = BlockStorage.getInventory(location);
+    if (menu == null || !menu.hasViewer()) {
+      return;
+    }
+
+    int storedCharge = data != null ? getCharge(location, data) : getCharge(location);
+
+    if (generated <= 0) {
+      menu.replaceExistingItem(
+          13,
+          CustomItemStack.create(
+              Material.RED_STAINED_GLASS_PANE,
+              "&cNot generating",
+              "&7Type: &6" + this.type,
+              "&7Stored: &6" + UtilEnergy.format(storedCharge) + " J",
+              "&7Capacity: &6" + UtilEnergy.format(this.buffer) + " J"
+          )
+      );
+    } else {
+      menu.replaceExistingItem(
+          13,
+          CustomItemStack.create(
+              Material.GREEN_STAINED_GLASS_PANE,
+              "&aGeneration",
+              "&7Type: &6" + this.type,
+              "&7Generating: &6" + UtilEnergy.format(generated) + " J/tick",
+              "&7Stored: &6" + UtilEnergy.format(storedCharge) + " J",
+              "&7Capacity: &6" + UtilEnergy.format(this.buffer) + " J"
+          )
+      );
+    }
   }
 
   private int getStoredValue(Config data, String key) {
@@ -137,10 +179,7 @@ public final class EnergyGenerator extends AbstractEnergyProvider {
 
     DEBUG_LOG_TIMESTAMPS.put(locationKey, now);
 
-    int storedCharge = 0;
-    if (data != null && isChargeable()) {
-      storedCharge = getCharge(location, data);
-    }
+    int storedCharge = data != null ? getCharge(location, data) : getCharge(location);
 
     Supreme.inst().log(Level.INFO,
         "[GeneratorDebug] id=" + getId()
